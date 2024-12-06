ed5903456a7e39a20d9659ebfe39409f0d676dcd
Grabemonster <exampel@exampel.com>
2024-12-06 22:46:49 +0100

    test

--- test.lua
+++ test.lua
@@ -1,6 +1,15 @@
 blocks_to_brake = {}
 
 function is_ore( block_data )
+    if block_data == nil then
+        return false
+    end
+    if block_data.name == nil then
+        return false
+    end
+    if block_data.tags == nil then
+        return false
+    end
     bool, n = block_data.tags["c:ores"]
     if bool then
         print(block_data.name)
