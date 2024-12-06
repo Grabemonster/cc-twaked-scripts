bd995286d61ab3bc210bf9ed4bb7746555a6e288
Grabemonster <exampel@exampel.com>
2024-12-06 23:09:27 +0100

    loade funktions better?

--- loade.lua
+++ loade.lua
@@ -1,7 +1,10 @@
-function loade( file_path )
-    shell.run("delete", file_path)
-    shell.run("wget", "https://raw.githubusercontent.com/Grabemonster/cc-twaked-scripts/refs/heads/master/" .. file_path, file_path)
+function loade(file_path)
+    shell.run("delete", file_path) -- Löscht die Datei lokal
+    local url = "https://raw.githubusercontent.com/Grabemonster/cc-twaked-scripts/refs/heads/master/" .. file_path
+    url = url .. "?t=" .. os.epoch("utc") -- Hängt den aktuellen Zeitstempel als Parameter an
+    shell.run("wget", url, file_path) -- Lädt die Datei neu herunter
 end
 
+-- Dateien herunterladen
 loade("test.lua")
 loade("loade.lua")
