0cb41abf3a290ce01ce5f9f3185e9151fe250b45
Grabemonster <exampel@exampel.com>
2024-12-06 22:36:44 +0100

    loade

--- loade.lua
+++ loade.lua
@@ -1,1 +1,7 @@
-shell.run("wget", "https://raw.githubusercontent.com/JustAnotherArchitect/Grabemonster/main/")
+function loade( file_path )
+    shell.run("delete", file_path)
+    shell.run("wget", "https://raw.githubusercontent.com/Grabemonster/cc-twaked-scripts/refs/heads/master/" .. file_path, file_path)
+end
+
+loade("loade.lua")
+loade("test.lua")
