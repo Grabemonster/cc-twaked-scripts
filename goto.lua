0ceb9a685e56fc3631a742f0934e12241bd2d1e5
Grabemonster <exampel@exampel.com>
2024-12-07 11:12:45 +0100

    test

--- goto.lua
+++ goto.lua
@@ -1,20 +1,20 @@
-blocks_to_break = {}
-cord = {x = 0, y = 0, z = 0} -- Enthält die Koordinaten und Blickrichtung
-direction = 0 -- North = 0, West = 1, South = 2, East = 3
+Blocks_to_break = {}
+Cord = {x = 0, y = 0, z = 0} -- Enthält die Koordinaten und Blickrichtung
+Direction = 0 -- North = 0, West = 1, South = 2, East = 3
 
-function dircetionNuumericToName(direction)
-    if direction == 0 then
+function DircetionNuumericToName(Direction)
+    if Direction == 0 then
         return "north"
-    elseif direction == 1 then
+    elseif Direction == 1 then
         return "west"
-    elseif direction == 2 then
+    elseif Direction == 2 then
         return "south"
-    elseif direction == 3 then
+    elseif Direction == 3 then
         return "east"
     end
 end
 
-function dircetionNameToNumeric(directionName)
+function DircetionNameToNumeric(directionName)
     if directionName == "north" then
         return 0
     elseif directionName == "west" then
@@ -27,8 +27,8 @@ function dircetionNameToNumeric(directionName)
 end
 
 -- Aktualisiert die Koordinaten basierend auf Blickrichtung
-function get_cords(directionName)
-    local new_cord = {x = cord.x, y = cord.y, z = cord.z}
+function Get_cords(directionName)
+    local new_cord = {x = Cord.x, y = Cord.y, z = Cord.z}
     if directionName == "north" then
         new_cord.z = new_cord.z - 1
     elseif directionName == "south" then
@@ -46,51 +46,51 @@ function get_cords(directionName)
 end
 
 -- Dreht die Schildkröte und aktualisiert die Blickrichtung
-function turn_right()
+function Turn_right()
     turtle.turnRight()
-    if direction == 0 then
-        direction = 3
+    if Direction == 0 then
+        Direction = 3
     else
-        direction = direction - 1
+        Direction = Direction - 1
     end
 
 end
 
-function turn_left()
+function Turn_left()
     turtle.turnLeft()
-    if direction == 3 then
-        direction = 0
+    if Direction == 3 then
+        Direction = 0
     else
-        direction = direction + 1
+        Direction = Direction + 1
     end
 end
 
-function moveforward( n )
+function MoveForward( n )
     n = n or 1
     for i = 1, n do
         turtle.forward()
-        cord = get_cords(dircetionNuumericToName(direction))
+        Cord = get_cords(dircetionNuumericToName(Direction))
     end
 end
 
-function moveup( n )
+function Move( n )
     n = n or 1
     for i = 1, n do
         turtle.up()
-        cord = get_cords(dircetionNuumericToName(direction))
+        Cord = get_cords(dircetionNuumericToName(Direction))
     end
 end
 
-function movedown( n )
+function MoveDown( n )
     n = n or 1
     for i = 1, n do
         turtle.down()
-        cord = get_cords(dircetionNuumericToName(direction))
+        Cord = get_cords(dircetionNuumericToName(Direction))
     end
 end
 
-function rotateTo( directionName )
-    divDirection = dircetionNameToNumeric(directionName) - direction
+function RotateTo( directionName )
+    local divDirection = dircetionNameToNumeric(directionName) - Direction
     if divDirection > 0 then
         for i = 1, divDirection do
             turn_right()
@@ -102,30 +102,32 @@ function rotateTo( directionName )
     end
 end
 
-function goTo( x, y, z)
-    divX = x - cord.x
-    divY = y - cord.y
-    divZ = z - cord.z
+function GoTo( x, y, z)
+    local divX = x - Cord.x
+    local divY = y - Cord.y
+    local divZ = z - Cord.z
 
     if divX > 0 then
-        rotateTo("east")
-        moveforward(divX)
+        RotateTo("east")
+        MoveForward(divX)
     elseif divX < 0 then
-        rotateTo("west")
-        moveforward(-divX)
+        RotateTo("west")
+        MoveForward(-divX)
     end
 
     if divY > 0 then
-        moveUp(divY)
+        Move(divY)
     elseif divY < 0 then
-        moveDown(-divY)
+        MoveDown(-divY)
     end
 
     if divZ > 0 then
-        rotateTo("south")
-        moveforward(divZ)
+        RotateTo("south")
+        MoveForward(divZ)
     elseif divZ < 0 then
-        rotateTo("north")
-        moveforward(-divZ)
+        RotateTo("north")
+        MoveForward(-divZ)
     end
 end
+
+GoTo(0, 2, 0)
