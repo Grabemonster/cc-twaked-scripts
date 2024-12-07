ada9bb758c82e36b27e6d9557896d354328cef01
Grabemonster <exampel@exampel.com>
2024-12-07 11:04:17 +0100

    goto test

--- goto.lua
+++ goto.lua
@@ -2,17 +2,6 @@ blocks_to_break = {}
 cord = {x = 0, y = 0, z = 0} -- Enthält die Koordinaten und Blickrichtung
 direction = 0 -- North = 0, West = 1, South = 2, East = 3
 
--- Überprüft, ob ein Block ein Erz ist
-function is_ore(block_data)
-    if not block_data or not block_data.name or not block_data.tags then
-        return false
-    end
-    if block_data.tags["c:ores"] then
-        return true
-    end
-    return false
-end
-
 function dircetionNuumericToName(direction)
     if direction == 0 then
         return "north"
@@ -76,39 +65,6 @@ function turn_left()
     end
 end
 
--- Scannt die Umgebung nach Erzen
-function scann()
-    -- Hilfsfunktion zum Überprüfen eines Blocks
-    local function check_block(inspect_func, cords)
-        local success, block_data = inspect_func()
-        if success and is_ore(block_data) then
-            table.insert(blocks_to_break, {cords = cords, block_data = block_data})
-            print("Erze gefunden! Koordinaten: " .. cords.x .. ", " .. cords.y .. ", " .. cords.z)
-        end
-    end
-
-    -- Scannt vorne
-    check_block(turtle.inspect, get_cords(dircetionNuumericToName(direction)))
-
-    -- Scannt unten
-    check_block(turtle.inspectDown, get_cords("down"))
-
-    -- Scannt oben
-    check_block(turtle.inspectUp, get_cords("up"))
-
-    -- Scannt rechts
-    turn_right()
-    check_block(turtle.inspect, get_cords(dircetionNuumericToName(direction)))
-
-    -- Scannt links
-    turn_left()
-    turn_left()
-    check_block(turtle.inspect, get_cords(dircetionNuumericToName(direction)))
-
-    -- Zurück zur ursprünglichen Richtung
-    turn_right()
-end
-
 function moveforward( n )
     n = n or 1
     for i = 1, n do
@@ -146,10 +102,6 @@ function rotateTo( directionName )
     end
 end
 
-function findNearest()
-    
-end
-
 function goTo( x, y, z)
     divX = x - cord.x
     divY = y - cord.y
@@ -175,29 +127,5 @@ function goTo( x, y, z)
     elseif divZ < 0 then
         rotateTo("north")
         moveforward(-divZ)
-    end
-end
-
--- Hauptschleife
-while true do
-    -- Überprüft den Treibstoff
-    if turtle.getFuelLevel() < 20 then
-        turtle.refuel(1)
-        print("Kein Treibstoff!")
-    end
-
-    -- Scannt nach Erzen
-    scann()
-
-    if #blocks_to_break > 0 then
-        findNearest()
-    end
-
-    -- Bewegt sich vorwärts und aktualisiert die Koordinaten
-    if turtle.forward() then
-        cord = get_cords(dircetionNuumericToName(direction))
-    else
-        print("Blockiert! Kann nicht vorwärts gehen.")
-        break -- Beendet die Schleife, wenn die Schildkröte blockiert ist
     end
 end
