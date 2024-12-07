7d716addcf56a3a4157a3fa01a486d2d1f29eebe
Grabemonster <exampel@exampel.com>
2024-12-07 20:15:44 +0100

    mine tow miners

--- mine.lua
+++ mine.lua
@@ -7,7 +7,20 @@ while true do
 
     turtle.dig()
     turtle.forward()
-    turtle.digUp()
+    turtle.turnLeft()
+    if not turtle.detect() then
+        turtle.select(2)
+        turtle.placeDown()
+        turtle.select(1)
+    end
+    turtle.turnRight()
+    turtle.turnRight()
+    if not turtle.detect() then
+        turtle.select(2)
+        turtle.placeDown()
+        turtle.select(1)
+    end
+    turtle.turnLeft()
     if not turtle.detectDown() then
         turtle.select(2)
         turtle.placeDown()
